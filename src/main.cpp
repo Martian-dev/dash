ad628da6087eaa6945cfcab841cf6225470a0002
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2025-02-27 17:35:10 +0530

    added echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+
+std::string parseCommand(std::stringstream *ss) {
+    std::string command;
+    *ss >> command;
+    return command;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -12,7 +19,17 @@ int main() {
 
       std::string input;
       std::getline(std::cin, input);
+      std::stringstream ss(input);
+      std::string command = parseCommand(&ss);
       if (input == "exit 0") break;
-      std::cout << input << ": command not found" << std::endl;
+      if (command == "echo") {
+          std::string word;
+          while (!ss.eof()) {
+              ss >> word;
+              std::cout << word << " ";
+          }
+          std::cout << std::endl;
+      }
+      else std::cout << input << ": command not found" << std::endl;
   }
 }
