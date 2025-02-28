c8458cf1efebd821a3cc3d6a39bb90893dcc21b1
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2025-02-28 22:53:15 +0530

    added type command

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,50 @@
 #include <sstream>
 #include <string>
 
+//enum BuiltinCommand { ECHO, EXIT, TYPE, INVALID };
+//
+//class Command {
+//private:
+//    BuiltinCommand type = INVALID;
+//
+//};
+
 std::string parseCommand(std::stringstream *ss) {
     std::string command;
     *ss >> command;
     return command;
+}
+
+void echo(std::stringstream *ss) {
+    std::string word;
+    while (!(*ss).eof()) {
+        *ss >> word;
+        std::cout << word << " ";
+    }
+    std::cout << std::endl;
+}
+
+void type(std::stringstream *ss) {
+    std::string word;
+    if (!(*ss).eof()) {
+        *ss >> word;
+        if (word == "echo") {
+            std::cout << "echo is a shell builtin" << std::endl;
+            return;
+        }
+        if (word == "exit") {
+            std::cout << "exit is a shell builtin" << std::endl;
+            return;
+        }
+        else {
+            std::cout << word + ": not found" << std::endl;
+            return;
+        }
+    }
+    else {
+        std::cout << "pass in an actual argument" << std::endl;
+        return;
+    }
 }
 
 int main() {
@@ -21,15 +61,18 @@ int main() {
       std::getline(std::cin, input);
       std::stringstream ss(input);
       std::string command = parseCommand(&ss);
-      if (input == "exit 0") break;
+//      std::cout << command << std::endl;
+      if (command == "exit") break;
       if (command == "echo") {
-          std::string word;
-          while (!ss.eof()) {
-              ss >> word;
-              std::cout << word << " ";
-          }
-          std::cout << std::endl;
+          echo(&ss);
+          continue;
       }
-      else std::cout << input << ": command not found" << std::endl;
+      if (command == "type") {
+          type(&ss);
+          continue;
+      }
+      else std::cout << command << ": command not found" << std::endl;
   }
+
+  exit(0);
 }
